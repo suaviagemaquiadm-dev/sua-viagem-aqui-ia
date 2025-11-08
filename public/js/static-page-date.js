1711f76edf59e08cc91d6a88c22b30ade3617c71
suaviagemaquiadm-dev <seu-email-do-github@exemplo.com>
2025-11-08 07:43:39 -0300

    Corrige configuração de deploy e adiciona firebase.json

--- public/js/static-page-date.js
+++
@@ -1,9 +0,0 @@
-/**
- * Populates the 'last-updated' element with the current date.
- */
-document.addEventListener("DOMContentLoaded", () => {
-    const dateElement = document.getElementById("last-updated");
-    if (dateElement) {
-      dateElement.textContent += ` ${new Date().toLocaleDateString("pt-BR")}`;
-    }
-});
