60e890ba52c803c713a4fa97e588b867d66a4919
suaviagemaquiadm-dev <seu-email-do-github@exemplo.com>
2025-11-14 15:09:06 -0300

    Fix:

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+
 /**
  * Ponto de entrada principal para todas as Cloud Functions.
  * O código foi refatorado para usar importações diretas, melhorando a clareza,
@@ -5,7 +6,7 @@
  */
 
 // Funções de Administração
-const adminFunctions = require("./src/admin");
+const adminFunctions = require("./admin");
 exports.createPartnerAccount = adminFunctions.createPartnerAccount;
 exports.deletePartnerAccount = adminFunctions.deletePartnerAccount;
 exports.setPartnerStatus = adminFunctions.setPartnerStatus;
@@ -14,56 +15,56 @@ exports.revokeAdminRole = adminFunctions.revokeAdminRole;
 exports.listAdmins = adminFunctions.listAdmins;
 
 // Funções de Contas
-const accountFunctions = require("./src/account");
-exports.generateAndAssignControlCode = accountFunctions.generateAndAssignControlCode;
-exports.sendPasswordResetEmail = accountFunctions.sendPasswordResetEmail;
+// const accountFunctions = require("./account");
+// exports.generateAndAssignControlCode = accountFunctions.generateAndAssignControlCode;
+// exports.sendPasswordResetEmail = accountFunctions.sendPasswordResetEmail;
 
 // Funções de Pagamentos
-const paymentFunctions = require("./src/payments");
+const paymentFunctions = require("./payments");
 exports.mercadoPagoWebhook = paymentFunctions.mercadoPagoWebhook;
-exports.createMercadoPagoPreference = paymentFunctions.createMercadoPagoPreference;
+// exports.createMercadoPagoPreference = paymentFunctions.createMercadoPagoPreference;
 
 // Funções de Usuários
-const userFunctions = require("./src/users");
+const userFunctions = require("./users");
 exports.updateUserProfile = userFunctions.updateUserProfile;
 exports.cleanupUserData = userFunctions.cleanupUserData;
-exports.toggleFollowUser = userFunctions.toggleFollowUser;
+// exports.toggleFollowUser = userFunctions.toggleFollowUser;
 
 // Funções de IA
-const aiFunctions = require("./src/ai");
-exports.generateItinerary = aiFunctions.generateItinerary;
-exports.suggestDestination = aiFunctions.suggestDestination;
+// const aiFunctions = require("./ai");
+// exports.generateItinerary = aiFunctions.generateItinerary;
+// exports.suggestDestination = aiFunctions.suggestDestination;
 
 // Funções de Contato
-const contactFunctions = require("./src/contact");
-exports.sendContactEmail = contactFunctions.sendContactEmail;
+// const contactFunctions = require("./contact");
+// exports.sendContactEmail = contactFunctions.sendContactEmail;
 
 // Funções do Chatbot
-const chatbotFunctions = require("./src/chatbot");
-exports.askChatbot = chatbotFunctions.askChatbot;
+// const chatbotFunctions = require("./chatbot");
+// exports.askChatbot = chatbotFunctions.askChatbot;
 
 // Funções de Avaliações (Reviews)
-const reviewFunctions = require("./src/reviews");
-exports.submitReview = reviewFunctions.submitReview;
-exports.updatePartnerRating = reviewFunctions.updatePartnerRating;
+// const reviewFunctions = require("./reviews");
+// exports.submitReview = reviewFunctions.submitReview;
+// exports.updatePartnerRating = reviewFunctions.updatePartnerRating;
 
 // Funções de Analytics
-const analyticsFunctions = require("./src/analytics");
-exports.trackProfileView = analyticsFunctions.trackProfileView;
-exports.trackWhatsappClick = analyticsFunctions.trackWhatsappClick;
+// const analyticsFunctions = require("./analytics");
+// exports.trackProfileView = analyticsFunctions.trackProfileView;
+// exports.trackWhatsappClick = analyticsFunctions.trackWhatsappClick;
 
 // Funções de Notificações
-const notificationFunctions = require("./src/notifications");
-exports.markNotificationsAsRead = notificationFunctions.markNotificationsAsRead;
+// const notificationFunctions = require("./notifications");
+// exports.markNotificationsAsRead = notificationFunctions.markNotificationsAsRead;
 
 // Funções de Configuração do Frontend
-const frontendConfigFunctions = require("./src/frontend_config");
-exports.getFrontendConfig = frontendConfigFunctions.getFrontendConfig;
+// const frontendConfigFunctions = require("./frontend_config");
+// exports.getFrontendConfig = frontendConfigFunctions.getFrontendConfig;
 
 // Funções de Busca
-const searchFunctions = require("./src/search");
-exports.searchPartners = searchFunctions.searchPartners;
+// const searchFunctions = require("./search");
+// exports.searchPartners = searchFunctions.searchPartners;
 
 // Funções da Página Inicial
-const partnerFunctions = require("./src/partners");
-exports.getHomePageData = partnerFunctions.getHomePageData;
+// const partnerFunctions = require("./partners");
+// exports.getHomePageData = partnerFunctions.getHomePageData;
